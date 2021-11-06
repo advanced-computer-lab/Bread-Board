6d52b055045b9e2ad21f8a77deaebd5f87be146f
BluBird4 <alhussienrehan@hotmail.com>
2021-11-06 10:48:31 +0200

    Added the db Schema to User and flights Models

--- backEnd/src/models/userModel.js
+++ backEnd/src/models/userModel.js
@@ -2,10 +2,23 @@ const mongoose = require ('mongoose')
 const Schema = mongoose.Schema ;
 const userSchema = new Schema({
 
-
 //string & types
-
-
+    name:{
+    type:String,
+    required:true
+    },
+    email:{
+    type:String,
+    required:true
+    },
+    password:{
+    type:String,
+    required:true
+    },
+    admin:{
+        type:Boolean,
+        required:false
+        }
 
 })
 
