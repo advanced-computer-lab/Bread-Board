d4e543925277df692c9c330098c15f166b13bfcd
Dahrawy828 <abdulrahman.aldahrawy@hotmail.com>
2021-11-07 14:46:42 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Component, useState, useEffect } from "react";
 import axios from "axios";
-import CreateFlight from "./Components/createFlight";
+import CreateFlight from "./Components/CreateFlight";
 import SearchFlight from "./Components/searchFlight";
 import DeleteFlight from "./Components/DeleteFlight";
 import UpdateFlight from "./Components/UpdateFlight";
 
 function App() {
-  return <UpdateFlight />;
+  return <CreateFlight />;
 }
 
 export default App;
