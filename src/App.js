8d4f440d950ff73d5da488f48601ce24c8d361a8
Dahrawy828 <abdulrahman.aldahrawy@hotmail.com>
2021-11-27 11:16:21 +0200

    Sprint 1 Final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import ManageFlights from "./Components/ManageFlights";
+import SearchFlights from "./Components/SearchFlights";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<ManageFlights />} />
+        <Route path="/admin/search" element={<SearchFlights />} />
       </Routes>
     </Router>
   );
